524c043feb3cee6f49d581c1cafdb92d4f819565
BrendonLH <Brendonhampton1988@gmail.com>
2022-08-30 20:31:38 -0700

    added models and filled in serializer

--- shark_conservation_app/admin.py
+++ shark_conservation_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Shark
 
-# Register your models here.
+
+my_models = [Shark]
+admin.site.register(Shark)
+
--- shark_conservation_app/models.py
+++ shark_conservation_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Shark(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(" ", blank=True)
+    location = models.TextField(" ", blank=True)
+    avg_length = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    diet = models.CharField(max_length=100, default=" ", blank=True)
+
+    def __str__(self):
+        return f"Name: {self.name}"
